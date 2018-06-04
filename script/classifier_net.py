c92e540215459dacc6cfe3cb7fb7cb84ee245fe0
kurff <kurffu@gmail.com>
2018-06-04 22:59:17 +0800

    script

---
+++ script/classifier_net.py
@@ -0,0 +1,34 @@
+import caffe 
+from caffe import layers as L 
+from caffe import params as P 
+import numpy as np 
+import argparse
+
+def base_net(prototxt):
+    prototxt_file=open(prototxt)
+    base_net_str=prototxt_file.read()
+    prototxt_file.close()
+    return base_net_str
+
+def inception_net(bottom_name='inception_4e/output', num_output = 62):
+
+    net=caffe.NetSpec()
+    net[bottom_name],net['label']=L.Data(ntop=2)
+    proto_str='%s'%net.to_proto()
+    return proto_str[proto_str.find('}')+1:]
+def write_file(file_name,proto_str):
+    text_file = open(file_name, "w")
+    text_file.write(proto_str)
+    text_file.close()
+
+
+if __name__ == '__main__':
+    base_str=base_net("train_val.prototxt");
+    last_str=base_str[base_str.rfind('top'):]
+    last_str=last_str[last_str.find('\"')+1:]
+    print last_str
+    feature_name=last_str[:last_str.find('\"')]
+    proto_str = inception_net(bottom_name=feature_name);
+    print proto_str
+    write_file("inception.prototxt", base_str+proto_str);
+
